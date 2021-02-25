3670a6db9704ab55631c1e4aae3f6afb9dbdca68
johnnysoto <johnny_soto@outlook.com>
2021-02-25 05:03:37 -0600

    first commit

---
+++ becoming-xcode-power-user/04/demos/module4/After/DateUtilities.framework/Headers/DateUtilities.h
@@ -0,0 +1,19 @@
+//
+//  DateUtilities.h
+//  DateUtilities
+//
+//  Created by Jonathan Wong on 5/18/20.
+//  Copyright © 2020 Fatty Waffles Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DateUtilities.
+FOUNDATION_EXPORT double DateUtilitiesVersionNumber;
+
+//! Project version string for DateUtilities.
+FOUNDATION_EXPORT const unsigned char DateUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateUtilities/PublicHeader.h>
+
+
