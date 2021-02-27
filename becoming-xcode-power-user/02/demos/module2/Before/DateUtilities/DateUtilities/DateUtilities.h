b4694cedde551d73cd8da0c6972774748419e28a
johnnysoto <johnny_soto@outlook.com>
2021-02-27 14:02:06 -0600

    .gitignore fix

---
+++ becoming-xcode-power-user/02/demos/module2/Before/DateUtilities/DateUtilities/DateUtilities.h
@@ -0,0 +1,18 @@
+//
+//  DateUtilities.h
+//  DateUtilities
+//
+//  Created by Johnny Soto on 2/25/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DateUtilities.
+FOUNDATION_EXPORT double DateUtilitiesVersionNumber;
+
+//! Project version string for DateUtilities.
+FOUNDATION_EXPORT const unsigned char DateUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateUtilities/PublicHeader.h>
+
+
